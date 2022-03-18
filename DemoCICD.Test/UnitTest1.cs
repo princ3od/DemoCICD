e5868e7df6b56d17f12f5f98c1c0f68f31952704
Trong <princ3od@gmail.com>
2022-03-18 19:51:38 +0700

    Add project files.

---
+++ DemoCICD.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+
+namespace DemoCICD.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [TestCase]
+        public void Test2()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ DemoCICD/DemoLogger.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoCICD
+{
+    public class DemoLogger
+    {
+        public static void Log()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
